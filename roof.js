accc2dcbc609830c1c8b4212930f867206399c8c
KaviGamer <69410834+KaviGamer@users.noreply.github.com>
2020-09-20 14:07:30 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,23 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      World.add(world, this.body);
+    }
+    display(){
+      push();
+      var pos =this.body.position;
+      translate(pos.x,pos.y);
+      rectMode(CENTER);
+      //strokeWeight(4);
+      fill(128,128,128);
+      rect(0,0,this.width,this.height);
+      pop();
+    }
+  }
---
+++ sketch.js
@@ -0,0 +1,76 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var bob1;
+var bob2;
+var bob3;
+var bob4;
+var bob5;
+var rope1;
+var rope2;
+var rope3;
+var rope4;
+var rope5;
+var world;
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+	//Create the Bodies Here.
+	
+	Engine.run(engine); 
+	roof = new Ground(width/2,175,229,20);
+	bobDiameter = 40;
+	startBPx = width/2;
+	startBPy = 475;
+	
+	bob1 = new Bob(startBPx-bobDiameter*2,startBPy,bobDiameter);
+	bob2 = new Bob(startBPx-bobDiameter,startBPy,bobDiameter);
+	bob3 = new Bob(startBPx,startBPy,bobDiameter);
+	bob4 = new Bob(startBPx+bobDiameter,startBPy,bobDiameter);
+	bob5 = new Bob(startBPx+bobDiameter*2,startBPy,bobDiameter);
+
+	var render = Render.create({
+		element:document.body,
+		engine:engine,
+		options:{
+		width:1200,
+		height:700,
+		wireframes:false	
+		}
+	});
+
+	rope1 = new Rope(bob1.body,roof.body,-bobDiameter*2,0);
+	rope2 = new Rope(bob2.body,roof.body,-bobDiameter,0);
+	rope3 = new Rope(bob3.body,roof.body,0,0);
+	rope4 = new Rope(bob4.body,roof.body,bobDiameter,0);
+	rope5 = new Rope(bob5.body,roof.body,bobDiameter*2,0);
+
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+  roof.display();
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  drawSprites();
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45});
+	}
+}
